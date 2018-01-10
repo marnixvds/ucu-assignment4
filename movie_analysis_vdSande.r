25752ca502726d7a01970c43f2e51445f5b888c2
Marnix van de Sande <m.a.j.vandesande@students.uu.nl>
2018-01-10 15:48:12 +0100

    Assignment 1 almost finished

--- movie_analysis_vdSande.r
+++ movie_analysis_vdSande.r
@@ -1,3 +1,20 @@
 library('tidyverse')
 
+#Read in and check out the data
 movies <- read.csv('movies.csv')
+summary(movies)
+#Interesting findings:
+#Date doesn't seem to be in the right formatting, which is probably necessary for putting it on the x-axis.
+#Genres are in dictionaries, which requires formatting for it to be usable in the graph
+movies$release_date[0] #That's right, date needs to be converted.
+
+#Formatting dates
+movies$release_date #How is it formatted
+as.Date(movies$release_date, '%Y-%m-%d') #Checkout before doing definitive operation
+movies$release_date <- as.Date(movies$release_date, '%Y-%m-%d')
+
+#Convert the budget data from factor to numerical.
+movies$budget <- as.numeric(as.character(movies$budget))
+#Filter out the movies for which a budget was specified
+movies.ofinterest <- movies %>% filter(budget > 10000 & budget < 3e+08 & release_date > '1940-01-01' & vote_count > 20)
+ggplot(data = movies.ofinterest) + geom_point(mapping = aes(x=release_date, y=budget, color=vote_average))
--- temp_analysis_vdSande.r
+++ temp_analysis_vdSande.r
@@ -1,3 +1,17 @@
 library('tidyverse')
 
+#Read in and check out the data
 temp <- read.csv('surfacetemp.csv')
+summary(temp)
+#How may cities are we taling about?
+unique(temp$city)
+#So 81 cities in the dataset
+
+#Are the dates correctly formatted?
+temp$dt[0]
+#Factor, so no! Let's format
+temp$dt #How is it formatted
+as.Date(movies$release_date, '%Y-%m-%d') #Checkout before doing definitive operation
+movies$release_date <- as.Date(movies$release_date, '%Y-%m-%d')
+
+ggplot(data = temp) + geom_point(mapping = aes(x=lng, y=lat, color=AverageTemperature))
